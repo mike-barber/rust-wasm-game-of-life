6942f9e27d712de549216fd16178053f9839512c
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2022-01-28 12:12:21 +0000

    used create-wasm-app template for `www`

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,12 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 extern {
+    // imported FROM JS so we can interact with it
     fn alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet() {
+    // exported TO JS so this function can be called
     alert("Hello, wasm-game-of-life!");
 }
