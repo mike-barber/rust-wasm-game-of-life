0a3c86b8dd113829ebb278f9d0a7d5957d38d6f7
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2022-09-21 08:59:23 +0100

    removed unmaintained wee_alloc

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,6 @@ use std::fmt::Display;
 
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 // A macro to provide `println!(..)`-style syntax for `console.log` logging.
 macro_rules! log {
     ( $( $t:tt )* ) => {
